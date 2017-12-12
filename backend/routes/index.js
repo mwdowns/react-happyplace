7d24e4753165964ae35b0f447798bbfb8332d035
Matthew Downs <mwdowns@gmail.com>
2017-12-12 15:15:58 -0500

    Adds a post method for creating a happyplace

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -32,4 +32,28 @@ router.get('/userhappyplaces/:username', function(req, res) {
     });
 });
 
+router.post('/createhappyplace', function(req, res) {
+    let username = req.body.username,
+        lat = req.body.lat,
+        lng = req.body.lng,
+        message = req.body.message;
+        user_id = 0;
+    db.task('create-happyplace', t=> {
+        return t.one('SELECT hp_user.id FROM hp_user WHERE hp_user.username = $1', username)
+        .then(user => {
+            user_id = user.id;
+            return t.one('INSERT INTO hp_location values (default, $1, $2, $3) RETURNING id', [lat, lng, message]);
+        })
+        .then(location => {
+            return t.one('INSERT INTO user_links_location values (default, $1, $2) RETURNING id', [user_id, location.id]);
+        })
+        .then(data => {
+            res.json({message: "data inserted"});
+        })
+    })
+    .catch(error => {
+        res.json({error: error});
+    })
+});
+
 module.exports = router;
